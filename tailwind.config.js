27b6d1a03844fd57a0ed760ff1763a11e6fb4311
Maciej Sudol <01161596@pw.edu.pl>
2023-12-28 13:09:37 +0100

    tailwinded

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./dist/*.{html,js,jsx}"],
   theme: {
+    colors: {
+      'dashboardcolor' : '#CFF4D2',
+      'activitycolor': '#7BE495',
+      'statisticscolor' : '#56C596',
+      'aiplannercolor' : '#329D9C',
+      'settingscolor' : '#3C6C8E'
+    },
     extend: {},
   },
   plugins: [],
