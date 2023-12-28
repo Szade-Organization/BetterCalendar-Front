9656c21aced1c4858dae14d0b4501ae49b2584ca
Bernard Cesarz <cesarzb@protonmail.com>
2023-12-28 13:07:07 +0100

    Accordion layout and cleaned up components

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./App.css";
-import AddActivity from "./components/AddActivity";
-import { AiPlannerWindow } from "./components/AiPlannerWindow/AiPlannerWindow.tsx";
 import Layout from "./components/Layout/Layout.jsx";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
-        <Route index element={<AddActivity />} />
-        <Route path="ai" element={<AiPlannerWindow />} />
+        {/* <Route index element={<AddActivity />} />
+        <Route path="ai" element={<AiPlannerWindow />} /> */}
       </Route>
     )
   );
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,55 @@
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "../../styles/Layout.css";
-import Navbar from "./Navbar";
 
 const Layout = () => {
+  const [activeSection, setActiveSection] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveSection(index === activeSection ? null : index);
+  };
+
+  const sections = [
+    {
+      title: "Your week",
+      content: "Your week dashboard here",
+      color: "#CFF4D2",
+    },
+    { title: "Activities", content: "Calendar here", color: "#7BE495" },
+    {
+      title: "Statistics",
+      content: "Statistics contents here",
+      color: "#56C596",
+    },
+    {
+      title: "AI Planner",
+      content: "AI planner contents here",
+      color: "#329D9C",
+    },
+    { title: "Settings", content: "Settings contents here", color: "#3C6C8E" },
+  ];
+
   return (
-    <div className="layout-container">
-      <Navbar />
-      <div className="layout-content">
-        <Outlet />
-      </div>
+    <div className="font-lato antialiased maximized">
+      <ul className="flex min-h-screen max-h-screen overflow-hidden list-none">
+        {sections.map((section, index) => (
+          <li
+            key={index}
+            className={`flex-item flex-${index + 1} ${
+              activeSection === index ? "active" : ""
+            }`}
+            style={{ background: section.color }}
+            onClick={() => toggleAccordion(index)}
+          >
+            <div className="section-title">
+              <h2>{section.title}</h2>
+            </div>
+            <div className="section-content">
+              <p>{section.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
