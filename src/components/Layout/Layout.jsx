f1bca70c2cadbf18dcdd7c52922fd384152a49a5
Bernard Cesarz <cesarzb@protonmail.com>
2024-01-05 19:06:12 +0100

    Kalendarz do którego można dodawać eventy

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import "../../styles/Layout.css";
 import AddActivity from "../AddActivity";
 import Statistics from "../Statistics";
+import BetterCalendar from "../BetterCalendar/BetterCalendar";
 
 const Layout = () => {
   const [activeSection, setActiveSection] = useState(null);
@@ -17,7 +18,11 @@ const Layout = () => {
       content: <AddActivity />,
       color: "#CFF4D2",
     },
-    { title: "Activities", content: "Calendar here", color: "#7BE495" },
+    {
+      title: "Activities",
+      content: <BetterCalendar />,
+      color: "#7BE495",
+    },
     {
       title: "Statistics",
       content: <Statistics />,
@@ -31,24 +36,26 @@ const Layout = () => {
     {
       title: "Settings",
       content: "Settings contents here",
-      color: "#3C6C8E"
+      color: "#3C6C8E",
     },
   ];
 
   return (
-
     <div className="font-lato antialiased maximized">
       <ul className="flex flex-row min-h-screen max-h-screen m-0 p-0 overflow-hidden list-none">
         {sections.map((section, index) => (
           <li
             key={index}
-            className={`flex-item cursor-pointer transition-flex duration-800 ease bg-activitycolor overflow-hidden flex-${index + 1} ${activeSection === index ? "active" : ""
-              }`}
+            className={`flex-item cursor-pointer transition-flex duration-800 ease bg-activitycolor overflow-hidden flex-${
+              index + 1
+            } ${activeSection === index ? "active" : ""}`}
             style={{ background: section.color }}
             onClick={() => toggleAccordion(index)}
           >
             <div className="section-title flex items-center justify-center m-5 p-0 text-center text-black font-Arial text-2xl max-w-25">
-              <h2 className="m-auto transform -rotate-90 whitespace-nowrap text-4xl font-bold">{section.title}</h2>
+              <h2 className="m-auto transform -rotate-90 whitespace-nowrap text-4xl font-bold">
+                {section.title}
+              </h2>
             </div>
             <div className="section-content flex items-center justify-center m-0 p-0 opacity-0 transition-all duration-250 delay-100 ease-out overflow-hidden">
               <p>{section.content}</p>
--- src/components/Ui/Modals/Modal.jsx
+++ src/components/Ui/Modals/Modal.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
 
 const Modal = ({ buttonText, content }) => {
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const closeModal = () => setShowModal(false);
+  const closeModal = () => setShowModal(false);
 
-    return (
+  return (
+    <>
+      <button
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        {buttonText}
+      </button>
+      {showModal ? (
         <>
-            <button
-                className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(true)}
-            >
-                {buttonText}
-            </button>
-            {showModal ? (
-                <>
-                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                        <div className="relative my-6 mx-auto w-full md:w-3/4 lg:w-1/2 max-w-[800px]">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                {content(closeModal)}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                </>
-            ) : null}
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative my-6 mx-auto w-full md:w-3/4 lg:w-1/2 max-w-[800px]">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {content(closeModal)}
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
-    );
+      ) : null}
+    </>
+  );
 };
 
 export default Modal;
