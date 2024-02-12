01d558a4e84c47aa54cb9932219275ba949699b3
Krzysztof Jastrzębski <krzysztof@jastrzebski>
2024-02-13 00:27:00 +0100

    podstawowe style kalendarza

--- src/components/BetterCalendar/BetterCalendar.jsx
+++ src/components/BetterCalendar/BetterCalendar.jsx
@@ -1,28 +1,14 @@
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useEffect, useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import Button from "../Ui/Buttons/Button";
 import Modal from "../Ui/Modals/Modal";
 import ActivityForm from "../Ui/Forms/ActivityForm";
 
+import CustomCalendar from "./CustomCalendar";
+
 const API_URL = import.meta.env.VITE_API_URL;
 
-const locales = {
-  "en-Us": "en-Us",
-};
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
 
 const BetterCalendar = () => {
   const [allEvents, setAllEvents] = useState([]);
@@ -35,12 +21,12 @@ const BetterCalendar = () => {
     date_start: new Date(event.date_start),
     date_end: new Date(event.date_end),
   });
-  
+
   const handleAddEvent = (newEvent) => {
     const eventWithDates = convertEventDates(newEvent);
     setAllEvents([...allEvents, eventWithDates]);
   };
-  
+
   const handleEditEvent = (eventId, updatedEvent) => {
     setAllEvents((currentEvents) =>
       currentEvents.map((event) =>
@@ -54,6 +40,7 @@ const BetterCalendar = () => {
     console.log(event);
     setShowDetails(true);
   }
+
   useEffect(() => {
     fetch(`${API_URL}/activity/`)
       .then((response) => response.json())
@@ -72,11 +59,9 @@ const BetterCalendar = () => {
   }, [allEvents]);
 
   return (
-    <div className="p-5 bg-white rounded-3xl">
+    <div className="p-3 bg-white rounded-3xl">
       <div className="flex justify-end mr-9">
-        <Button className="bg-lime-400" onClick={() => setShowAddForm(true)}>
-          Add event
-        </Button>
+      
       </div>
       {showAddForm &&
         <Modal
@@ -87,14 +72,15 @@ const BetterCalendar = () => {
           />}
         />
       }
-      <Calendar
-        localizer={localizer}
+      <CustomCalendar    
         events={allEvents}
         titleAccessor="name"
         startAccessor="date_start"
         endAccessor="date_end"
-        style={{ height: 500, margin: 30 }}
-        onSelectEvent={handleEventSelect}
+        style={{ width: 600, height: 600, margin: 20 }}
+        onSelectEvent={handleEventSelect}  
+        setShowAddForm={setShowAddForm}
+        handleEventSelect={handleEventSelect}      
       />
       {showDetails && (
         <Modal
@@ -106,6 +92,7 @@ const BetterCalendar = () => {
               handleAddEvent={handleAddEvent}
               handleEditEvent={handleEditEvent}
               isEditing={true}
+
             />}
         />
       )}
