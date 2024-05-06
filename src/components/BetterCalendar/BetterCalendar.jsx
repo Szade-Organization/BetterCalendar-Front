f0d243616ac245e6593330eb8644021433c29ae7
Bernard Cesarz <cesarzb@protonmail.com>
2024-05-06 20:02:30 +0200

    Unified styles between accordion screens

--- src/components/BetterCalendar/BetterCalendar.jsx
+++ src/components/BetterCalendar/BetterCalendar.jsx
@@ -48,7 +48,7 @@ const BetterCalendar = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="p-5 bg-white rounded-3xl w-full">
+      <div className="p-5 bg-white rounded-3xl min-w-full max-w-xl h-[90%]">
         {showAddForm && (
           <Modal
             content={
